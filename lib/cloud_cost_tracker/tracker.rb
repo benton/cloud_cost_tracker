f2f93889066ce15463088cdc4807a40f733f1c7f
Benton Roberts <benton@bentonroberts.com>
2012-01-29 19:47:38 -0500

    Delegate most of CoudTracker::Tracker's public methods to FogTracker::Tracker

--- lib/cloud_cost_tracker/tracker.rb
+++ lib/cloud_cost_tracker/tracker.rb
@@ -1,68 +1,52 @@
 module CloudCostTracker
 
-  # Tracks one or more Fog accounts in an ActiveRecord database
+  # Tracks the resources in one or more Fog accounts in an ActiveRecord database
   class Tracker
-    require 'fog_tracker'
+    
+    # each CloudCostTracker is a thin wrapper around it's @tracker,
+    require 'fog_tracker'     # which is a FogTracker:Tracker
+    extend Forwardable        # most public methods are delegated
+    def_delegators :@tracker,:start,:stop,:query,:[],
+            :running?,:types_for_account,:logger
 
-    # Creates an object for tracking Fog accounts
-    #
-    # ==== Attributes
-    #
-    # * +accounts+ - a Hash of account information (see accounts.yml.example)
-    # * +options+ - Hash of optional parameters
-    #
-    # ==== Options
-    #
-    # * +:delay+ - Time between polling of accounts. Overrides per-account value
-    # * +:log+ - a Ruby Logger-compatible object
+    # Creates an object for tracking Fog accounts in an ActiveRecord database
+    # @param [Hash] accounts a Hash of account information
+    #    (see accounts.yml.example)
+    # @param [Hash] options optional additional parameters:
+    #  - :delay (Integer) - Default time between polling of accounts
+    #  - :error_callback (Proc) - A Method or Proc to call if polling errors occur.
+    #    (should take a single Exception as its only required parameter)
+    #  - :logger - a Ruby Logger-compatible object
     def initialize(accounts = {}, options={})
       @accounts = accounts
       @delay    = options[:delay]
-      @log      = options[:logger] || CloudCostTracker.default_logger
+      @log      = options[:logger] || FogTracker.default_logger
       setup_fog_tracker
+      @running  = false
     end
+
+    private
 
     # Creates a FogTracker::Tracker that calls bill_for_account
     # each time an account is refreshed
     def setup_fog_tracker
-      callback = Proc.new do |account_name, *args|
-        bill_for_account(account_name)
-      end
       @tracker = FogTracker::Tracker.new(@accounts,
-        {:delay => @delay, :logger => @log, :callback => callback}
+        {:delay => @delay, :logger => @log,
+          :callback => Proc.new do |resources|
+            bill_for_resources(resources)
+          end
+        }
       )
     end
 
-    # Invokes the start method on all the @trackers
-    def start
-      if not running?
-        @tracker.start
-        @running = true
-      else
-        @log.info "Already tracking #{@trackers.keys.count} accounts"
-      end
-    end
-
-    # Invokes the stop method on all the @trackers
-    def stop
-      if running?
-        @tracker.stop
-        @running = false
-      else
-        @log.info "Tracking already stopped"
-      end
-    end
-
-    # Returns true or false/nil depending on whether this tracker is polling
-    def running? ; @running end
-
     # Generates BillingRecords for all Resources in account named +account_name+
-    def bill_for_account(account_name)
-      @log.info "Generating cost info for account #{account_name}"
-      @tracker.types_for_account(account_name).each do |type|
-        @log.debug "Generating cost info for #{type} on account #{account_name}"
-        account = @accounts[account_name]
-      end
+    def bill_for_resources(resources)
+      account_name = resources.first.tracker_account[:name]
+      @log.debug "Generating cost info for account #{account_name}"
+      biller = Billing::AccountBillingPolicy.new(
+        account_name, @accounts[account_name], { :logger => @log }
+      )
+      biller.bill_resources(resources)
     end
   end
 end
