cc57372e33639906fca0c50401de69f94fee9463
Benton Roberts <benton@bentonroberts.com>
2012-01-29 21:20:31 -0500

    Simplify default BillingPolicy

--- lib/cloud_cost_tracker/billing/default_policy.rb
+++ lib/cloud_cost_tracker/billing/default_policy.rb
@@ -4,33 +4,19 @@ module CloudCostTracker
     module Resources
       class ResourceBillingPolicy
 
-        # Creates an object for billing Fog resources
-        #
-        # ==== Attributes
-        #
-        # * +account_name+ - the name of the account to bill for
-        # * +account+ - the Hash of account information (see accounts.yml.example)
-        # * +options+ - Hash of optional parameters
-        #
-        # ==== Options
-        #
-        # * +:logger+ - a Ruby Logger-compatible object
-        def initialize(account_name, account, options={})
-          @account_name = account_name
-          @account      = account
-          @log          = options[:logger] || CloudCostTracker.default_logger
+        # Creates an object that implements a default (zero-cost) billing policy
+        # @param [Hash] options optional parameters:
+        #  - :logger - a Ruby Logger-compatible object
+        def initialize(options={})
+          @log = options[:logger] || FogTracker.default_logger
         end
 
         # returns the cost for a particular resource since a given point in time
         # if this returns -1, get_cost_for_time will be called instead
-        def get_cost_since_time(resource, date_time)
-          -1
-        end
+        def get_cost_since_time(resource, date_time) ; nil end
 
         # returns the cost for a particular resource over some duration (in seconds)
-        def get_cost_for_duration(resource, duration)
-          0.0
-        end
+        def get_cost_for_duration(resource, duration) ; 0.0 end
 
       end
     end
