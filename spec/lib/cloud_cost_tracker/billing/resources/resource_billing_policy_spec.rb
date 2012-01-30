b892af8e4701c2fbdbd0c92433ae0c4b6ba8a97b
Benton Roberts <broberts@mdsol.com>
2012-01-30 17:50:29 -0500

    Fix ResourceBillingPolicy tests

--- spec/lib/cloud_cost_tracker/billing/resources/resource_billing_policy_spec.rb
+++ spec/lib/cloud_cost_tracker/billing/resources/resource_billing_policy_spec.rb
@@ -4,19 +4,19 @@ module CloudCostTracker
       describe ResourceBillingPolicy do
 
         before(:each) do
-          @default_policy = ResourceBillingPolicy.new
-          @resource = double "a mock Fog::Model resource"
+          @resource = Fog::Compute[:aws].servers.new
+          @default_policy = ResourceBillingPolicy.new(@resource)
         end
 
         describe '#get_cost_since_time' do
           it 'should always return nil, even with nil arguments' do
-            @default_policy.get_cost_since_time(nil, nil).should == nil
+            @default_policy.get_cost_since_time(nil).should == nil
           end
         end
 
         describe '#get_cost_since_time' do
           it 'should always return zero cost, even with nil arguments' do
-            @default_policy.get_cost_for_duration(nil, nil).should == 0.0
+            @default_policy.get_cost_for_duration(nil).should == 0.0
           end
         end
 
