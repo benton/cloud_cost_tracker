c5ba45e81b109cf321b84cc70125fe69fc6ef2d5
Benton Roberts <broberts@mdsol.com>
2012-01-30 18:07:05 -0500

    Test CloudCostTracker::create_billing_agent

---
+++ spec/lib/cloud_cost_tracker/cloud_cost_tracker_spec.rb
@@ -0,0 +1,17 @@
+module CloudCostTracker
+  describe '#create_billing_agent' do
+    context "with a resource whose BillingPolicy class is defined" do
+      it "returns an instance of the ResourceBillingPolicy subclass" do
+        CloudCostTracker::create_billing_agent(Fog::Compute[:aws].servers.new).
+          should be_an_instance_of(
+            Billing::Resources::Compute::AWS::ServerBillingPolicy
+          )
+      end
+    end
+    context "with a resource whose BillingPolicy class is not defined" do
+      it "returns nil" do
+        CloudCostTracker::create_billing_agent(double("random")).should == nil
+      end
+    end
+  end
+end
