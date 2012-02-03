032078bf0e41a70d093d3c77e70645e258c0b997
Benton Roberts <benton@bentonroberts.com>
2012-02-03 08:16:43 -0500

    First pass at EC2 volumes BillingPolicy

--- lib/cloud_cost_tracker/billing/resources/compute/aws/snapshots.rb
+++ lib/cloud_cost_tracker/billing/resources/compute/aws/snapshots.rb
@@ -5,7 +5,7 @@ module CloudCostTracker
         module AWS
           class SnapshotBillingPolicy < ResourceBillingPolicy
             # returns the cost for a particular resource over some duration (in seconds)
-            def get_cost_for_duration(duration)
+            def get_cost_for_duration(resource, duration)
               2.0
             end
           end
--- lib/cloud_cost_tracker/billing/resources/compute/aws/volumes.rb
+++ lib/cloud_cost_tracker/billing/resources/compute/aws/volumes.rb
@@ -9,8 +9,14 @@ module CloudCostTracker
               CONSTANTS_DIR, 'compute-aws-volumes.yml'))
 
             # returns the cost for a particular resource over some duration (in seconds)
-            def get_cost_for_duration(duration)
-              CENTS_PER_GB_PER_MONTH * volume_size_in_gb * duration / SECONDS_PER_MONTH
+            def get_cost_for_duration(resource, duration)
+              CENTS_PER_GB_PER_MONTH[zone(resource)] * resource.size *
+                duration / SECONDS_PER_MONTH
+            end
+
+            # chop the availability zone lette from the region
+            def zone(resource)
+              resource.availability_zone.chop
             end
           end
         end
