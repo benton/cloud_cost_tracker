e7314b3811db71cfbcdd802732f03b5039e529fc
Benton Roberts <broberts@mdsol.com>
2012-02-03 13:08:54 -0500

    Attempt to compute zone for EBS snapshots

--- lib/cloud_cost_tracker/billing/resources/compute/aws/snapshots.rb
+++ lib/cloud_cost_tracker/billing/resources/compute/aws/snapshots.rb
@@ -9,17 +9,25 @@ module CloudCostTracker
               CONSTANTS_DIR, 'compute-aws-snapshots.yml'))
 
             # returns the cost for a particular resource over some duration (in seconds)
+            # TODO - AWS does not seem to report the size of the snapshot, so there's
+            # no way to verify the cost!  :(
+            # This code is only accurate if the snapshot is the first for its volume
             def get_cost_for_duration(resource, duration)
-              CENTS_PER_GB_PER_MONTH[zone(resource)] * resource.size *
+              CENTS_PER_GB_PER_MONTH[zone(resource)] * resource.volume_size *
                 duration / SECONDS_PER_MONTH
             end
 
             # Follow the snapshot's volume and return its region
             # chop the availability zone letter from the region
             def zone(resource)
-              (resource.account_resources('volumes').select do |v|
+              volume = resource.account_resources('volumes').find do |v|
                 v.identity == resource.volume_id
-              end).first.availability_zone.chop
+              end
+              if volume && volume.availability_zone
+                volume.availability_zone.chop
+              else
+                "us-east-1"
+              end
             end
 
           end
