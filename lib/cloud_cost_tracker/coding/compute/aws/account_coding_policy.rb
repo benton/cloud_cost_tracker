186d10efe57f9c8167592080a13180d1613ae5e4
Benton Roberts <benton@bentonroberts.com>
2012-02-11 16:52:16 -0500

    Implement AWS EC2 account-wide coding policy

--- lib/cloud_cost_tracker/coding/compute/aws/account_coding_policy.rb
+++ lib/cloud_cost_tracker/coding/compute/aws/account_coding_policy.rb
@@ -2,7 +2,7 @@ module CloudCostTracker
   module Coding
     module Compute
       module AWS
-        # Defines the default order in which resources get "coded"
+        # Defines the default order in which EC2 resources get "coded"
         class AccountCodingPolicy < CloudCostTracker::Coding::AccountCodingPolicy
 
           # Defines the order in which EC2 resources are coded
@@ -17,6 +17,15 @@ module CloudCostTracker
               Fog::Compute::AWS::Volume,
               Fog::Compute::AWS::Address,
             ]
+          end
+
+          # Acount-wide coding strategy for coding EC2 resources
+          def attach_account_codes(resource)
+            if resource.respond_to?(:tags) and resource.tags
+              resource.tags.each do |key, value|
+                resource.code(key, value)
+              end
+            end
           end
 
         end
--- lib/cloud_cost_tracker/coding/compute/aws/servers.rb
+++ lib/cloud_cost_tracker/coding/compute/aws/servers.rb
@@ -4,13 +4,8 @@ module CloudCostTracker
       module AWS
         class ServerCodingPolicy < ResourceCodingPolicy
 
-          # Attaches Billing Codes (String pairs) to resource, as billing_codes
+          # Coding policy for EC2 servers
           def code(aws_server)
-            if aws_server.tags
-              aws_server.tags.each do |key, value|
-                aws_server.code(key, value)
-              end
-            end
           end
 
         end
