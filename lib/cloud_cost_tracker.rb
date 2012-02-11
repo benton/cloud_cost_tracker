435c6e27e2039eea4e4143b8470aba969e4de1bc
Benton Roberts <benton@bentonroberts.com>
2012-02-11 11:26:07 -0500

    Implements CloudCostTracker#create_coding_agents

--- lib/cloud_cost_tracker.rb
+++ lib/cloud_cost_tracker.rb
@@ -1,8 +1,9 @@
 require 'active_record'
 require 'logger'
 
-# Load ResourceBillingPolicy class
+# Load generic ResourceBillingPolicy and Coding classes
 require 'cloud_cost_tracker/billing/resources/resource_billing_policy'
+require 'cloud_cost_tracker/coding/resources/resource_coding_policy'
 # Load all ruby files from 'cloud_cost_tracker' directory
 Dir[File.join(File.dirname(__FILE__), "cloud_cost_tracker/**/*.rb")].each {|f| require f}
 
@@ -26,6 +27,33 @@ module CloudCostTracker
           # Search through the classes in the module for all matches
           classes = provider_module.classes.each do |policy_class|
             if policy_class.name =~ /#{model_name}.*BillingPolicy/
+              agents << policy_class.new(:logger => options[:logger])
+            end
+          end
+        end
+      end
+    end
+    agents
+  end
+
+  # Creates and returns an Array of ResourceCodingPolicy (subclass) instances
+  # for coding the given +resource+, or an empty Array of none are found
+  # @param [Class] resource_class the Class object for the billed resource
+  # @param [Hash] options optional additional parameters:
+  #  - :logger - a Ruby Logger-compatible object
+  # @return [Array <ResourceCodingPolicy>] coding policy objects for resource
+  def self.create_coding_agents(resource_class, options = {})
+    agents = Array.new
+    # Safely descend through the Coding module Heirarchy
+    if matches = resource_class.name.match(%r{^Fog::(\w+)::(\w+)::(\w+)})
+      fog_svc, provider, model_name = matches[1], matches[2], matches[3]
+      if CloudCostTracker::Coding::Resources.const_defined? fog_svc
+        service_module = CloudCostTracker::Coding::Resources::const_get fog_svc
+        if service_module.const_defined? provider
+          provider_module = service_module.const_get provider
+          # Search through the classes in the module for all matches
+          classes = provider_module.classes.each do |policy_class|
+            if policy_class.name =~ /#{model_name}.*CodingPolicy/
               agents << policy_class.new(:logger => options[:logger])
             end
           end
--- spec/lib/cloud_cost_tracker/cloud_cost_tracker_spec.rb
+++ spec/lib/cloud_cost_tracker/cloud_cost_tracker_spec.rb
@@ -1,7 +1,10 @@
 module CloudCostTracker
+
   describe '#create_billing_agents' do
     context "for a resource with only a single BillingPolicy defined" do
       it "returns an Array with the correct ResourceBillingPolicy subclass" do
+        CloudCostTracker::create_billing_agents(FAKE_AWS.servers.new.class).
+          first.class.should == Billing::Resources::Compute::AWS::ServerBillingPolicy
       end
     end
     context "for a resource with several BillingPolicy classes" do
@@ -18,4 +21,28 @@ module CloudCostTracker
       end
     end
   end
+
+  describe '#create_coding_agents' do
+    context "for a resource with only a single CodingPolicy defined" do
+      it "returns an Array with the correct ResourceCodingPolicy subclass" do
+        CloudCostTracker::create_coding_agents(FAKE_AWS.servers.new.class).
+          first.class.should == Coding::Resources::Compute::AWS::ServerCodingPolicy
+      end
+    end
+    context "for a resource with several CodingPolicy classes" do
+      it "returns an Array with the correct ResourceCodingPolicy subclass" do
+        pending("need for multiple coding policies per resource")
+        agents = CloudCostTracker::create_coding_agents(FAKE_RDS.servers.new.class)
+        agent_classes = agents.map {|agent| agent.class.name.split('::').last}
+        agent_classes.should include 'ServerCodingPolicy'
+        agent_classes.should include 'ServerStorageCodingPolicy'
+      end
+    end
+    context "for a resource whose CodingPolicy class is not defined" do
+      it "returns an empty Array" do
+        CloudCostTracker::create_coding_agents(double("x").class).should == []
+      end
+    end
+  end
+
 end
