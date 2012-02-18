25483bd2d052319536271dcc6e8903eaf8074737
Benton Roberts <benton@bentonroberts.com>
2012-02-18 15:05:16 -0500

    Comment out pending test

--- spec/lib/cloud_cost_tracker/cloud_cost_tracker_spec.rb
+++ spec/lib/cloud_cost_tracker/cloud_cost_tracker_spec.rb
@@ -25,18 +25,18 @@ module CloudCostTracker
   describe '#create_coding_agents' do
     context "for a resource with only a single CodingPolicy defined" do
       it "returns an Array with the correct ResourceCodingPolicy subclass" do
-        CloudCostTracker::create_coding_agents(FAKE_AWS.servers.new.class).
-          first.class.should == Coding::Compute::AWS::ServerCodingPolicy
+        CloudCostTracker::create_coding_agents(FAKE_AWS.volumes.new.class).
+          first.class.should == Coding::Compute::AWS::VolumeCodingPolicy
       end
     end
     context "for a resource with several CodingPolicy classes" do
-      it "returns an Array with the correct ResourceCodingPolicy subclass" do
-        pending("need for multiple coding policies per resource")
-        agents = CloudCostTracker::create_coding_agents(FAKE_RDS.servers.new.class)
-        agent_classes = agents.map {|agent| agent.class.name.split('::').last}
-        agent_classes.should include 'ServerCodingPolicy'
-        agent_classes.should include 'ServerStorageCodingPolicy'
-      end
+      #it "returns an Array with the correct ResourceCodingPolicy subclass" do
+      #  pending("need for multiple coding policies per resource")
+      #  agents = CloudCostTracker::create_coding_agents(FAKE_RDS.servers.new.class)
+      #  agent_classes = agents.map {|agent| agent.class.name.split('::').last}
+      #  agent_classes.should include 'ServerCodingPolicy'
+      #  agent_classes.should include 'ServerStorageCodingPolicy'
+      #end
     end
     context "for a resource whose CodingPolicy class is not defined" do
       it "returns an empty Array" do
