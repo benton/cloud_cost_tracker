61003d9e58ffd37be88454e0d83d884ec485daa8
Benton Roberts <benton@bentonroberts.com>
2012-02-18 15:30:30 -0500

    Propagate billing codes from EC2 servers to their attached volumes

--- lib/cloud_cost_tracker/coding/compute/aws/volumes.rb
+++ lib/cloud_cost_tracker/coding/compute/aws/volumes.rb
@@ -7,6 +7,16 @@ module CloudCostTracker
 
           # Copies all billing codes from any attached instance to this volume
           def code(ebs_volume)
+            if (ebs_volume.state == "in-use") && (ebs_volume.server_id != "")
+              server = ebs_volume.account_resources('servers').find do |server|
+                server.identity == ebs_volume.server_id
+              end
+              if server
+                server.billing_codes.each do |server_code|
+                  ebs_volume.code(server_code[0], server_code[1])
+                end
+              end
+            end
           end
 
         end
