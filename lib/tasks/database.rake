8d8221babbd49bf1384216dd4571492208b8f5c4
Benton Roberts <benton@bentonroberts.com>
2012-03-10 09:25:51 -0500

    Adjust DB config file search logic in tests

--- lib/tasks/database.rake
+++ lib/tasks/database.rake
@@ -1,5 +1,3 @@
-DEFAULT_CONFIG_FILE = File.expand_path(
-  '../../../config/database.example.yml', __FILE__)
 require 'active_record'
 
 namespace :db do
@@ -15,9 +13,7 @@ namespace :db do
     end
 
     task :db_connect do
-      config_file = DEFAULT_CONFIG_FILE
-      config_file = './config/database.yml' if File.exists? './config/database.yml'
-      config_file = ENV['DB_CONFIG_FILE'] if ENV['DB_CONFIG_FILE']
+      config_file = ENV['DB_CONFIG_FILE'] || './config/database.yml'
       puts "Using database config file #{config_file}..."
       config = YAML::load(File.open(config_file))[ENV['RACK_ENV'] || 'development']
       puts "Using database #{config['database']}..."
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,37 +1,45 @@
-ENV['RACK_ENV'] = 'test'
+ENV['RACK_ENV'] ||= 'test'
 Bundler.require # Load all gems and libs
 
 Fog.mock!       # Don't try to connect to the network
-module CloudCostTracker
-  FAKE_ACCOUNT_NAME = 'Fake EC2 Account'
-  FAKE_ACCOUNT = {
-    :name         => FAKE_ACCOUNT_NAME,
-    :provider     => 'AWS',
-    :service      => 'Compute',
-    :delay        => 10,
-    :credentials  => {
-      :aws_access_key_id => "fake user",
-      :aws_secret_access_key => 'fake password'
-    },
-    :exclude_resources => [
-      :spot_requests,   # No Fog mocks for this resource
-    ],
-  }
-  FAKE_ACCOUNTS = {FAKE_ACCOUNT_NAME => FAKE_ACCOUNT}
-  FAKE_AWS = Fog::Compute.new(
-    :provider => 'AWS',
-    :aws_access_key_id => 'XXXXXXXXXXXXXXXXXXXX',
-    :aws_secret_access_key => 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
-  )
-  FAKE_RDS = Fog::AWS::RDS.new(
-    :aws_access_key_id => 'XXXXXXXXXXXXXXXXXXXX',
-    :aws_secret_access_key => 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
-  )
-end
 
-## Establish ActiveRecord connection
-db_conf_file = File.expand_path('../../config/database.example.yml', __FILE__)
-db_conf_file = ENV['DB_CONFIG_FILE'] if ENV['DB_CONFIG_FILE']
+FAKE_ACCOUNT_NAME = 'Fake EC2 Account'
+FAKE_ACCOUNT = {
+  :name         => FAKE_ACCOUNT_NAME,
+  :provider     => 'AWS',
+  :service      => 'Compute',
+  :delay        => 10,
+  :credentials  => {
+    :aws_access_key_id => "fake user",
+    :aws_secret_access_key => 'fake password'
+  },
+  :exclude_resources => [
+    :spot_requests,   # No Fog mocks for this resource
+  ],
+}
+FAKE_ACCOUNTS = {FAKE_ACCOUNT_NAME => FAKE_ACCOUNT}
+FAKE_AWS = Fog::Compute.new(
+  :provider => 'AWS',
+  :aws_access_key_id => 'XXXXXXXXXXXXXXXXXXXX',
+  :aws_secret_access_key => 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+)
+FAKE_RDS = Fog::AWS::RDS.new(
+  :aws_access_key_id => 'XXXXXXXXXXXXXXXXXXXX',
+  :aws_secret_access_key => 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+)
+
+## Establish ActiveRecord connection and Migrate database
+# The following db config files are used, in order of preference:
+#   1. ENV['DB_CONFIG_FILE']
+#   2. ./config/database.yml
+#   3. ./config/database.example.yml
+db_conf_file = './config/database.example.yml'
+db_conf_file = './config/database.yml' if File.exists? './config/database.yml'
+if ENV['DB_CONFIG_FILE']
+  db_conf_file = ENV['DB_CONFIG_FILE']
+else
+  ENV['DB_CONFIG_FILE'] = db_conf_file
+end
 puts "Reading DB config file #{db_conf_file}..."
 db_config = YAML::load(File.open(db_conf_file))[ENV['RACK_ENV'] || 'test']
 puts "Using DB #{db_config['database']}..."
